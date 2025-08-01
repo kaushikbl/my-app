b0c245027b61338afaab88e37c5a5c3395328cba
kaushikbl <kaushikblokesh@gmail.com>
2025-08-01 19:30:52 +0530

    updated

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -4,7 +4,7 @@ export default function Dashboard() {
   return (
     <div>
       <h2>🏗️ House Construction </h2>
-      <p>Budget: 120000</p>
+      <p>Budget: 12007700</p>
       <p>Tasks in Progress: 3</p>
       <p>Materials Ordered: 12</p>
     </div>
