6d7b6497818f63e6db224b454f80e34f6f36236d
kaushikbl <kaushikblokesh@gmail.com>
2025-08-05 11:59:53 +0530

    updts

--- backend/server.js
+++ backend/server.js
@@ -12,3 +12,7 @@ mongoose.connect(process.env.MONGO_URI)
   })
   .catch(err => console.error(err));
 
+  app.listen(5000, '0.0.0.0', () => {
+  console.log('Server is running on port 5000');
+});
+
