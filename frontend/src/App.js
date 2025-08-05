be75fd86abf88c12f7210c8125fa4b85786e1904
kaushikbl <kaushikblokesh@gmail.com>
2025-08-05 17:10:19 +0530

    xx

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,6 @@ import Dashboard from './components/Dashboard';
 import TaskForm from './components/TaskForm';
 import MaterialForm from './components/MaterialForm';
 import ExpenseForm from './components/ExpenseForm';
-import Login from './pages/Login';
 
 function App() {
   return (
